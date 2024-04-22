f83131c2829c46c60d59fe90d3d727c9b4e66927
Christian Smith Mantas <casm202@gmail.com>
2024-04-23 01:14:16 +0200

    Init: start of vanilla project.

---
+++ 1-vanilla-js/src/components/Sidebar.js
@@ -0,0 +1,61 @@
+/**
+ * Sidebar component
+ */
+export class Sidebar {
+    navLinks = [];
+
+    /**
+     * Constructor for the sidebar component
+     * @param { name: string, href: string }[] links 
+     */
+    constructor(links) {
+        this.navLinks = links;
+    }
+
+    /**
+     * Method to render sidebar links
+     * @param { name: string, href: string }[] link 
+     * @returns string
+     */
+    link(link) {
+
+        const isActive = window.location.pathname === link.href;
+        const activeClass = isActive ? 'active' : null;
+        
+        return `
+            <a href="${link.href}" class="navigation-link ${activeClass}">
+                <span>${link.name}</span>
+            </a>
+        `;
+    };
+
+    /**
+     * Method to render sidebar component
+     * @returns string
+     */
+    render() {
+
+        return `
+            <!-- Sidebar component -->
+            <aside class="sidebar-styled">
+
+                <!-- Sidebar logo -->
+                <div class="sidebar-logo">
+                    <a href="/">
+                        <img src="./public/logo.svg" alt="">
+                    </a>
+                </div>
+
+                <!-- Sidebar navigation -->
+                <nav class="sidebar-navigation">
+                    ${this.navLinks.map(link => this.link(link)).join('')}
+                </nav>
+
+                <!-- Sidebar footer -->
+                <footer class="sidebar-footer">
+                    <p>Framework Movies ©️ 2024</p>
+                </footer>
+            </aside>  
+        `;
+    };
+};
---
+++ 1-vanilla-js/src/scripts/index.js
@@ -0,0 +1,126 @@
+// Component imports
+import { Sidebar } from "../components/Sidebar.js";
+import { Page } from "../components/page.js";
+
+// Page imports
+import { MoviesPage } from "../pages/Movies.js";
+import { TVSeriesPage } from "../pages/TVSeries.js";
+import { AnimePage } from "../pages/Anime.js";
+import { FavouritesPage } from "../pages/Favourites.js";
+import { Error404Page } from "../pages/Error404.js";
+
+// Sidebar links
+const sidebarLinks = [
+    {
+        name: 'Movies',
+        href: '/'
+    },
+    {
+        name: 'TV Seires',
+        href: '/tv-series'
+    },
+    {
+        name: 'Anime',
+        href: '/anime'
+    },
+    {
+        name: 'Favourites',
+        href: '/favourites'
+    },
+];
+
+
+/**
+ * Method for artificial link routing.
+ */
+const handleLinkClick = (event) => {
+    
+    // Prevent default link functionality
+    event.preventDefault();
+
+    // Retrieve link href and push to history
+    const href = event.currentTarget.getAttribute('href');
+    window.history.pushState({}, '', href);
+};
+
+/**
+ * Method to replace links with artificial routing
+ */
+const replaceLinks = () => {
+    const links = document.querySelectorAll('a');
+    links.forEach(link => {
+        link.addEventListener('click', handleLinkClick);
+    });
+};
+
+/**
+ * Method that renders the page
+ */
+const renderPage = () => {
+    const path = window.location.pathname;
+    let page;
+    let content;
+
+    switch (path) {
+        case '/': {
+            content = new MoviesPage();
+            break;
+        }
+        case '/tv-series': {
+            content = new TVSeriesPage();
+            break;
+        }
+        case '/anime': {
+            content = new AnimePage();
+            break;
+        }
+        case '/favourites': {
+            content = new FavouritesPage();
+            break;
+        }
+        default: {
+            content = new Error404Page();
+            console.log('error here bud');
+            break;
+        }
+    };
+
+    page = new Page(content.getHtml(), content.title, content.description);
+    document.querySelector('#render-page').innerHTML = page.render();
+};
+
+/**
+ * Listener for page load.
+ */
+window.onload = () => {
+    
+    // Render sidebar
+    const sidebar = new Sidebar(sidebarLinks);
+    document.querySelector('#render-sidebar').outerHTML = sidebar.render();
+
+    // Replace links
+    replaceLinks();
+
+    // Render page
+    renderPage(window.location.pathname);
+};
+
+/**
+ * Listen for pathname changes
+ */
+window.addEventListener('click', e => {
+    const isLink = e.target.closest('a')?.tagName === 'A';
+
+    if (isLink) {
+
+        // Re-render page
+        renderPage();
+
+        // Re-render navigation links
+        const newLinks = sidebarLinks.map(link => new Sidebar().link(link)).join('');
+        document.querySelector('.sidebar-navigation').innerHTML = newLinks;
+
+        // Replace links
+        replaceLinks();
+    };
+});
