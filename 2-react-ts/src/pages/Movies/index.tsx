37a1ce476d7bc4b110528bcbb8b28b4c3ae71f9c
Christian Smith Mantas <casm202@gmail.com>
2024-05-14 21:31:56 +0200

    Create: content card component with styles.

--- 2-react-ts/src/pages/Movies/index.tsx
+++ 2-react-ts/src/pages/Movies/index.tsx
@@ -1,8 +1,27 @@
+// Component imports
+import { ContentCard } from "../../components/ContentCard";
+
+
 export default function Movies() {
 
     return (
-        <>
-            <h2>Movies</h2>
-        </>
+        <div className="page-wrapper">
+
+            {/* Page header */}
+            <div className="page-header">
+                <p className="header-title">Movies</p>
+                <p className="header-sum">420</p>
+            </div>
+
+            {/* Content grid */}
+            <div className="content-grid">
+                <ContentCard
+                    id="321"
+                    title="Hot Fuzz"
+                    tags={['action', 'comedy']}
+                />
+            </div>
+
+        </div>
     );
 };
